d6a218c14b59a9ac759d89c5a6316651291607eb
RicPet87 <richard_pettersson@hotmail.com>
2026-01-26 13:58:18 +0100

    routing for smithsignup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Knives from "./pages/Knives";
+import SmithSignup from "./pages/SmithSignup";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/knives" element={<Knives />} />
+        <Route path="/smithsignup" element={<SmithSignup />} />
       </Routes>
     </BrowserRouter>
   );
