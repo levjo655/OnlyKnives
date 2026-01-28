f5d7bc55f35edf75760b360f4d8435aed96aa2e4
MattyMags <mattmag18@gmail.com>
2026-01-27 21:31:39 -0800

    wip: add some helpful comments

--- frontend/src/components/core-ui/index.js
+++ frontend/src/components/core-ui/index.js
@@ -1,2 +1,4 @@
+// This file is the entry point for the core-ui folder.
+// It exports all the components from the core-ui folder.
 export { Footer } from './footer';
 export { Navbar } from './navbar';
