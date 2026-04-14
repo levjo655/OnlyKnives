869a453c6865b16d6974dd6b08fe22dcac690f48
Levjo Cibuku <levjo,cibuku@.com>
2026-04-14 10:00:27 +0200

    feat: updated ports

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     setIsLoading(true);
 
     try {
-      const res = await fetch("http://localhost:23456/users/login", {
+      const res = await fetch("http://localhost:8080/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
